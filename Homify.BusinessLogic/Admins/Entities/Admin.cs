55a86d15fbd055a398889b6d15a48a1564f145a7
Guillermo Dotti <diottiguillermo@gmail.com>
2024-09-13 20:26:45 -0300

    refactoring classes User and Admin

--- Homify.BusinessLogic/Admins/Entities/Admin.cs
+++ Homify.BusinessLogic/Admins/Entities/Admin.cs
@@ -4,16 +4,7 @@ namespace Homify.BusinessLogic.Admins.Entities;
 
 public class Admin : User
 {
-    public string Name { get; set; } = null!;
-    public string Email { get; set; } = null!;
-    public string Password { get; set; } = null!;
-    public string LastName { get; set; } = null!;
-    public string Id { get; init; }
-    public DateTimeOffset CreatedAt { get; init; }
-
     public Admin()
     {
-        Id = Guid.NewGuid().ToString();
-        CreatedAt = DateTimeOffset.UtcNow;
     }
 }
