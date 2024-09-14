c06ebc041cb084a157ca513c466ff0f2e0bed6b7
Guillermo Dotti <diottiguillermo@gmail.com>
2024-09-14 00:51:40 -0300

    adding coverage to reposiroty tests

--- Homify.Tests/RepositoriesTests/RepositoryTests.cs
+++ Homify.Tests/RepositoriesTests/RepositoryTests.cs
@@ -2,6 +2,7 @@ using FluentAssertions;
 using Homify.DataAccess.Contexts;
 using Homify.DataAccess.Contexts.TestContext;
 using Homify.DataAccess.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace Homify.Tests.RepositoriesTests;
 
@@ -69,6 +70,109 @@ public class RepositoryTests
         var entitySaved = entitiesSaved[0];
         entitySaved.Id.Should().Be(expectedEntity.Id);
         entitySaved.Name.Should().Be(expectedEntity.Name);
+    }
+    #endregion
+
+    #region Get
+    [TestMethod]
+    public void Get_WhenEntityExists_ShouldReturnEntity()
+    {
+        var entity = new EntityTest("existing entity");
+        _context.Add(entity);
+        _context.SaveChanges();
+
+        var result = _repository.Get(e => e.Id == entity.Id);
+
+        result.Should().NotBeNull();
+        result.Id.Should().Be(entity.Id);
+        result.Name.Should().Be(entity.Name);
+    }
+
+    [TestMethod]
+    public void Get_WhenEntityDoesNotExist_ShouldThrowInvalidOperationException()
+    {
+        var nonExistentId = Guid.NewGuid().ToString();
+
+        Action action = () => _repository.Get(e => e.Id == nonExistentId);
+
+        action.Should().Throw<InvalidOperationException>()
+            .WithMessage($"Entity {nameof(EntityTest)} not found");
+    }
+
+    #endregion
+
+    #region Exist
+    [TestMethod]
+    public void Exist_WhenEntityExists_ShouldReturnTrue()
+    {
+        var entity = new EntityTest("existing entity");
+        _context.Add(entity);
+        _context.SaveChanges();
+
+        var result = _repository.Exist(e => e.Name == "existing entity");
+
+        result.Should().BeTrue();
+    }
+
+    [TestMethod]
+    public void Exist_WhenEntityDoesNotExist_ShouldReturnFalse()
+    {
+        var result = _repository.Exist(e => e.Name == "non-existing entity");
+
+        result.Should().BeFalse();
+    }
+    #endregion
+
+    #region Remove
+    [TestMethod]
+    public void Remove_WhenEntityExists_ShouldRemoveEntity()
+    {
+        var entity = new EntityTest("entity to remove");
+        _context.Add(entity);
+        _context.SaveChanges();
+
+        _repository.Remove(entity);
+
+        _context.EntitiesTest.FirstOrDefault(e => e.Id == entity.Id).Should().BeNull();
+    }
+
+    [TestMethod]
+    public void Remove_WhenEntityDoesNotExist_ShouldThrowException()
+    {
+        var entity = new EntityTest("non-existing entity");
+
+        Action action = () => _repository.Remove(entity);
+
+        action.Should().Throw<DbUpdateConcurrencyException>();
+    }
+    #endregion
+
+    #region Update
+    [TestMethod]
+    public void Update_WhenEntityExists_ShouldUpdateEntity()
+    {
+        var entity = new EntityTest("old name");
+        _context.Add(entity);
+        _context.SaveChanges();
+
+        _context.Entry(entity).State = EntityState.Detached;
+
+        entity = new EntityTest { Id = entity.Id, Name = "new name" };
+        _repository.Update(entity);
+
+        var updatedEntity = _context.EntitiesTest.AsNoTracking().FirstOrDefault(e => e.Id == entity.Id);
+        updatedEntity.Should().NotBeNull();
+        updatedEntity!.Name.Should().Be("new name");
+    }
+
+    [TestMethod]
+    public void Update_WhenEntityDoesNotExist_ShouldThrowException()
+    {
+        var entity = new EntityTest { Id = Guid.NewGuid().ToString(), Name = "non-existing entity" };
+
+        Action action = () => _repository.Update(entity);
+
+        action.Should().Throw<DbUpdateConcurrencyException>();
     }
     #endregion
 }
