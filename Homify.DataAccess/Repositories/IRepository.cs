815c7404edd1b7e244153fb396f91bc6f96e08cd
Guillermo Dotti <diottiguillermo@gmail.com>
2024-09-13 23:10:52 -0300

    creating Repository and IRepository and installing nugget packages

---
+++ Homify.DataAccess/Repositories/IRepository.cs
@@ -0,0 +1,19 @@
+using System.Linq.Expressions;
+
+namespace Homify.DataAccess.Repositories;
+
+public interface IRepository<TEntity>
+    where TEntity : class
+{
+    bool Exist(Expression<Func<TEntity, bool>> expression);
+
+    void Add(TEntity entity);
+
+    List<TEntity> GetAll(Expression<Func<TEntity, bool>>? predicate = null);
+
+    TEntity Get(Expression<Func<TEntity, bool>> predicate);
+
+    void Remove(TEntity entity);
+
+    void Update(TEntity entity);
+}
