90b1aaffcbd4c12b063f9a9b4a08b2842e76cdb5
juanpeyrot <106559717+juanpeyrot@users.noreply.github.com>
2024-09-17 15:28:00 -0300

    create company red

---
+++ Homify.Tests/ControllerTests/CompanyControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using Homify.BusinessLogic.Users;
+using Homify.WebApi.Controllers.Admins;
+using Moq;
+
+namespace Homify.Tests.ControllerTests;
+
+[TestClass]
+public class CompanyControllerTest
+{
+    [TestMethod]
+    public void Create_WhenDataIsOk_ShouldCreateCompany()
+    {
+        var request = CreateCompanyRequest();
+        var expected = new Company();
+
+        var response = _controller.Create(request);
+
+        response.Should().NotBeNull();
+        response.Id.Should().NotBeNull();
+        response.Id.Should().NotBeEmpty();
+        response.Id.Should().Be(expected.Id);
+    }
+}
