1c56bd67d85a3fd8556a7b7c41efa9220e3f6883
juanpeyrot <106559717+juanpeyrot@users.noreply.github.com>
2024-09-25 18:00:24 -0300

    database setup

--- Homify.DataAccess/Contexts/HomifyDbContext.cs
+++ Homify.DataAccess/Contexts/HomifyDbContext.cs
@@ -1,11 +1,51 @@
+using Homify.BusinessLogic.Cameras.Entities;
+using Homify.BusinessLogic.Companies;
+using Homify.BusinessLogic.CompanyOwners;
+using Homify.BusinessLogic.Devices;
+using Homify.BusinessLogic.HomeDevices;
+using Homify.BusinessLogic.Homes.Entities;
+using Homify.BusinessLogic.HomeUsers;
+using Homify.BusinessLogic.HouseOwner;
+using Homify.BusinessLogic.Notifications.Entities;
+using Homify.BusinessLogic.Sensors.Entities;
+using Homify.BusinessLogic.Sessions.Entities;
+using Homify.BusinessLogic.Users.Entities;
 using Homify.DataAccess.Contexts.TestContext;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 
 namespace Homify.DataAccess.Contexts;
 
-public class HomifyDbContext : DbContext
+public sealed class HomifyDbContext : DbContext
 {
+    public DbSet<Camera> Cameras { get; set; }
+    public DbSet<Company> Companies { get; set; }
+    public DbSet<CompanyOwner> CompanyOwners { get; set; }
+    public DbSet<Device> Devices { get; set; }
+    public DbSet<HomeDevice> HomeDevice { get; set; }
+    public DbSet<HomeOwner> HomeOwner { get; set; }
+    public DbSet<Home> Homes { get; set; }
+    public DbSet<HomeUser> HomeUser { get; set; }
+    public DbSet<Notification> Notifications { get; set; }
+    public DbSet<Sensor> Sensors { get; set; }
+    public DbSet<Session> Sessions { get; set; }
+    public DbSet<User> Users { get; set; }
+
+    public HomifyDbContext(DbContextOptions options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<HomeDevice>()
+            .HasKey(hd => new { hd.HomeId, hd.DeviceId });
+        modelBuilder.Entity<HomeUser>()
+            .HasKey(hu => new { hu.HomeId, hu.UserId });
+
+        base.OnModelCreating(modelBuilder);
+    }
+
     private static readonly SqliteConnection _connection = new("Data Source=:memory:");
 
     public static TestDbContext BuildTestDbContext()
@@ -17,7 +57,6 @@ public class HomifyDbContext : DbContext
         _connection.Open();
 
         var context = new TestDbContext(options);
-
         return context;
     }
 }
