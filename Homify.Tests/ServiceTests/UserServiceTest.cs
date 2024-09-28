705028193018800fa254273b93532ff082c79aad
Nicolas Toscano <toscanico3@gmail.com>
2024-09-28 20:49:57 -0300

    green add user when info is ok

---
+++ Homify.Tests/ServiceTests/UserServiceTest.cs
@@ -0,0 +1,56 @@
+using Homify.BusinessLogic.Users;
+using Homify.BusinessLogic.Users.Entities;
+using Homify.DataAccess.Repositories;
+using Homify.DataAccess.Repositories.Roles;
+using Moq;
+
+namespace Homify.Tests.ServiceTests;
+
+[TestClass]
+public class UserServiceTest
+{
+    private Mock<IRepository<User>>? _userRepositoryMock;
+    private Mock<UserService>? _userServiceMock;
+
+    private UserService? _service;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _userRepositoryMock = new Mock<IRepository<User>>();
+        _userServiceMock = new Mock<UserService>(_userRepositoryMock.Object);
+        _service = new UserService(_userRepositoryMock.Object);
+    }
+
+    [TestMethod]
+    public void AddUser_WhenInfoIsOk_ShouldAddUserToRepository()
+    {
+        var mockRepository = new Mock<IRepository<User>>();
+        var userService = new UserService(mockRepository.Object);
+        Role rol = new Role();
+        var createUserArgs = new CreateUserArgs(
+              "John",
+              "john@example.com",
+              "password123!",
+              "Doe",
+              rol
+        );
+
+        var result = _service.AddUser(createUserArgs);
+
+        _userRepositoryMock.Verify(r => r.Add(It.Is<User>(u =>
+            u.Name == createUserArgs.Name &&
+            u.Email == createUserArgs.Email &&
+            u.Password == createUserArgs.Password &&
+            u.LastName == createUserArgs.LastName &&
+            u.Role == createUserArgs.Role
+        )), Times.Once);
+
+        Assert.IsNotNull(result);
+        Assert.AreEqual(createUserArgs.Name, result.Name);
+        Assert.AreEqual(createUserArgs.Email, result.Email);
+        Assert.AreEqual(createUserArgs.Password, result.Password);
+        Assert.AreEqual(createUserArgs.LastName, result.LastName);
+        Assert.AreEqual(createUserArgs.Role, result.Role);
+    }
+}
