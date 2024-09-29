1098d39d82ce81aef47335c511bb631b66e14749
Guillermo Dotti <diottiguillermo@gmail.com>
2024-09-29 13:23:03 -0300

    fix/session coverage

--- Homify.BusinessLogic/Sessions/Entities/Session.cs
+++ Homify.BusinessLogic/Sessions/Entities/Session.cs
@@ -3,16 +3,18 @@
 namespace Homify.BusinessLogic.Sessions.Entities;
 public class Session
 {
-    public int Id { get; set; }
+    public string Id { get; init; }
     public Guid AuthToken { get; set; }
     public User User { get; set; } = new User();
 
     public Session()
     {
+        Id = Guid.NewGuid().ToString();
     }
 
     public Session(Guid token, User user)
     {
+        Id = Guid.NewGuid().ToString();
         AuthToken = token;
         User = user;
     }
--- Homify.Tests/ControllerTests/SessionControllerTest.cs
+++ Homify.Tests/ControllerTests/SessionControllerTest.cs
@@ -1,8 +1,11 @@
 ﻿using FluentAssertions;
 using Homify.BusinessLogic.Sessions;
+using Homify.BusinessLogic.Sessions.Entities;
 using Homify.BusinessLogic.Users;
+using Homify.BusinessLogic.Users.Entities;
 using Homify.Exceptions;
 using Homify.WebApi.Controllers.Session;
+using Homify.WebApi.Controllers.Session.Models;
 using Moq;
 
 namespace Homify.Tests.ControllerTests;
@@ -26,5 +29,102 @@ public class SessionControllerTest
     public void Create_WhenRequestIsNull_ShouldThrowException()
     {
         _controller.Create(null);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgsNullException))]
+    public void Create_WhenEmailIsNull_ShouldThrowArgsNullException()
+    {
+        var request = new CreateSessionRequest
+        {
+            Email = null,
+            Password = "testPassword"
+        };
+
+        _controller.Create(request);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(NotFoundException))]
+    public void Create_WhenUserNotFound_ShouldThrowNotFoundException()
+    {
+        var request = new CreateSessionRequest
+        {
+            Email = "nonexistent@example.com",
+            Password = ".Coo120dshjha"
+        };
+
+        _userServiceMock.Setup(us => us.GetAll())
+            .Returns(new List<User>());
+
+        _controller.Create(request);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidFormatException))]
+    public void Create_WhenPasswordIsIncorrect_ShouldThrowArgumentException()
+    {
+        var request = new CreateSessionRequest
+        {
+            Email = "user@example.com",
+            Password = "wrongPassword"
+        };
+
+        var user = new User
+        {
+            Email = "user@example.com",
+            Password = ".Coo120dshjha"
+        };
+
+        _userServiceMock.Setup(us => us.GetAll())
+            .Returns(new List<User> { user });
+
+        _controller.Create(request);
+    }
+
+    [TestMethod]
+    public void Create_WhenCredentialsAreCorrect_ShouldReturnCreateSessionResponse()
+    {
+        var request = new CreateSessionRequest
+        {
+            Email = "user@example.com",
+            Password = ".Coo120dshjha"
+        };
+
+        var user = new User
+        {
+            Email = "user@example.com",
+            Password = ".Coo120dshjha"
+        };
+
+        Guid authToken = Guid.NewGuid();
+        Guid authToken2 = Guid.NewGuid();
+
+        var session = new Session(authToken, user);
+
+        var sesion2 = new Session
+        {
+            AuthToken = authToken2,
+            User = user
+        };
+
+        _userServiceMock.Setup(us => us.GetAll())
+            .Returns(new List<User>
+            {
+                user
+            });
+
+        _sessionServiceMock.Setup(ss => ss.AddToken(It.IsAny<string>()))
+            .Returns(session);
+
+        var result = _controller.Create(request);
+
+        Assert.IsNotNull(result);
+        result.Should().BeOfType<CreateSessionResponse>();
+        authToken.Should().Be(authToken.ToString());
+        session.Id.Should().NotBe(null);
+        session.User.Should().Be(user);
+        sesion2.User.Should().Be(user);
+        sesion2.AuthToken.Should().Be(authToken2);
     }
 }
