2d02581edacf8f5713784f03838f685f553d033b
juanpeyrot <106559717+juanpeyrot@users.noreply.github.com>
2024-09-30 18:02:27 -0300

    green authorizationfilter Authenticate_WhenTokenIsCorrect_ShouldAuthenticate

---
+++ Homify.Tests/FilterTests/AuthorizationFilterAttributeTest.cs
@@ -0,0 +1,101 @@
+﻿using FluentAssertions;
+using Homify.BusinessLogic.Roles;
+using Homify.BusinessLogic.Sessions;
+using Homify.BusinessLogic.SystemPermissions;
+using Homify.BusinessLogic.Users.Entities;
+using Homify.Utility;
+using Homify.WebApi;
+using Homify.WebApi.Filters;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Primitives;
+using Moq;
+using RouteData = Microsoft.AspNetCore.Routing.RouteData;
+
+namespace Homify.Tests.FilterTests;
+
+[TestClass]
+public class AuthorizationFilterAttributeTest
+{
+    private Mock<HttpContext> _httpContextMock;
+    private Mock<ISessionService> _sessionServiceMock;
+    private AuthorizationFilterContext _context;
+    private AuthorizationFilterAttribute _attribute = null!;
+
+    private SystemPermission _permission;
+    private Role _roleForTest;
+    private User _user;
+    private string _validToken;
+    private string _authorizationHeader;
+
+    public AuthorizationFilterAttributeTest()
+    {
+        _httpContextMock = new Mock<HttpContext>(MockBehavior.Loose);
+        _sessionServiceMock = new Mock<ISessionService>(MockBehavior.Loose);
+        _context = new AuthorizationFilterContext(
+            new ActionContext(
+                _httpContextMock.Object,
+                new RouteData(),
+                new ActionDescriptor()
+            ),
+            new List<IFilterMetadata>()
+        );
+
+        _permission = new SystemPermission { Value = "admins-Create" };
+
+        _roleForTest = new Role { Name = Constants.ADMINISTRATOR, Permissions = new List<SystemPermission> { _permission } };
+
+        _authorizationHeader = "Authorization";
+        _validToken = Guid.NewGuid().ToString();
+        _user = new User
+        {
+            Name = "Hola",
+            LastName = "ADA",
+            Email = "ada@email.com",
+            Password = "adaPASS",
+            Role = _roleForTest
+        };
+
+        _sessionServiceMock.Setup(sessionService =>
+            sessionService.GetUserByToken(_validToken)).Returns(_user);
+        _httpContextMock.Setup(h => h.Request.Headers[_authorizationHeader]).Returns(_validToken);
+        _httpContextMock.Setup(h => h.RequestServices.GetService(It.IsAny<Type>()))
+            .Returns(_sessionServiceMock.Object);
+        _httpContextMock.SetupSet(h => h.Items[Items.UserLogged] = _user);
+        _httpContextMock.Setup(h => h.Items[Items.UserLogged]).Returns(_user);
+        _attribute = new AuthorizationFilterAttribute("admins-Create");
+    }
+
+    [TestMethod]
+    public void Authenticate_WhenTokenIsCorrect_ShouldAuthenticate()
+    {
+        var AUTHORIZATION_HEADER = "Authorization";
+        var validToken = Guid.NewGuid().ToString();
+        var user = new User
+        {
+            Name = "BBB",
+            LastName = "AAA",
+            Email = "BB@email.com",
+            Password = "bbPASS",
+        };
+
+        _sessionServiceMock.Setup(sessionService => sessionService.GetUserByToken(validToken)).Returns(user);
+
+        _httpContextMock.Setup(h => h.Request.Headers.TryGetValue(AUTHORIZATION_HEADER, out It.Ref<StringValues>.IsAny))
+            .Returns((string key, out StringValues value) =>
+            {
+                value = new StringValues(validToken);
+                return true;
+            });
+
+        _httpContextMock.Setup(h => h.RequestServices.GetService(typeof(ISessionService)))
+            .Returns(_sessionServiceMock.Object);
+
+        _attribute.OnAuthorization(_context);
+
+        IActionResult? response = _context.Result;
+        response.Should().BeNull();
+    }
+}
---
+++ Homify.WebApi/Filters/AuthorizationFilterAttribute.cs
@@ -0,0 +1,49 @@
+﻿using System.Net;
+using Homify.BusinessLogic.SystemPermissions;
+using Homify.BusinessLogic.Users.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Homify.WebApi.Filters;
+
+public sealed class AuthorizationFilterAttribute : AuthenticationFilterAttribute
+{
+    public string Code { get; set; }
+
+    public AuthorizationFilterAttribute(string code)
+    {
+        Code = code;
+    }
+
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        base.OnAuthorization(context);
+
+        User userLogged = (User)context.HttpContext.Items[Items.UserLogged];
+
+        if (userLogged != null && !HasPermission(userLogged, Code))
+        {
+            context.Result = new ObjectResult(new
+            {
+                InnerCode = "Unauthorized",
+                Message = "You are not authorized"
+            })
+            {
+                StatusCode = (int)HttpStatusCode.Forbidden
+            };
+        }
+    }
+
+    private bool HasPermission(User userLogged, string code)
+    {
+        foreach (SystemPermission permission in userLogged.Role.Permissions)
+        {
+            if (permission.Value == code)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
