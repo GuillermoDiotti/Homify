daf241159d289fc11c4e83b8f3e8383960029ffe
juanpeyrot <106559717+juanpeyrot@users.noreply.github.com>
2024-10-02 21:42:20 -0300

    home repository

---
+++ Homify.DataAccess/Repositories/HomeRepository.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq.Expressions;
+using Homify.BusinessLogic.Homes.Entities;
+using Homify.Exceptions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Homify.DataAccess.Repositories;
+
+public class HomeRepository : Repository<Home>
+{
+    public HomeRepository(DbContext context)
+        : base(context)
+    {
+    }
+
+    public override Home? Get(Expression<Func<Home, bool>> predicate)
+    {
+        var query =
+            _entities.Include(u => u.Devices)
+                .Include(u => u.Owner)
+                .Where(predicate);
+
+        var home = query.FirstOrDefault();
+
+        if (home == null)
+        {
+            throw new NotFoundException($"Home not found");
+        }
+
+        return home;
+    }
+}
