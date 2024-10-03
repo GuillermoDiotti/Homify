b6fe0e09b2192c0e78210e78fa8b7cbdc3247933
juanpeyrot <106559717+juanpeyrot@users.noreply.github.com>
2024-10-03 01:36:08 -0300

    green homeuser service getbyids

---
+++ Homify.BusinessLogic/HomeUsers/HomeUserService.cs
@@ -0,0 +1,23 @@
+﻿using Homify.DataAccess.Repositories;
+
+namespace Homify.BusinessLogic.HomeUsers;
+
+public class HomeUserService : IHomeUserService
+{
+    private readonly IRepository<HomeUser> _repository;
+
+    public HomeUserService(IRepository<HomeUser> repository)
+    {
+        _repository = repository;
+    }
+
+    public HomeUser? GetByIds(string? homeId, string? userId)
+    {
+        return _repository.Get(x => x.HomeId == homeId && x.UserId == userId);
+    }
+
+    public HomeUser Update(HomeUser hu)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Homify.Tests/ServiceTests/HomeUserServiceTest.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq.Expressions;
+using Homify.BusinessLogic.HomeUsers;
+using Homify.DataAccess.Repositories;
+using Moq;
+
+namespace Homify.Tests.ServiceTests;
+
+[TestClass]
+public class HomeUserServiceTest
+{
+    private Mock<IRepository<HomeUser>>? _mockRepository;
+    private HomeUserService? _homeUserService;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _mockRepository = new Mock<IRepository<HomeUser>>();
+        _homeUserService = new HomeUserService(_mockRepository.Object);
+    }
+
+    [TestMethod]
+    public void GetByIds_ShouldReturnHomeUser_WhenHomeUserExists()
+    {
+        var homeId = "home123";
+        var userId = "user123";
+
+        var expectedHomeUser = new HomeUser
+        {
+            HomeId = homeId,
+            UserId = userId,
+        };
+
+        _mockRepository.Setup(r => r.Get(It.IsAny<Expression<Func<HomeUser, bool>>>()))
+            .Returns(expectedHomeUser);
+
+        var result = _homeUserService.GetByIds(homeId, userId);
+
+        Assert.IsNotNull(result);
+        Assert.AreEqual(expectedHomeUser.HomeId, result.HomeId);
+        Assert.AreEqual(expectedHomeUser.UserId, result.UserId);
+        _mockRepository.Verify(r => r.Get(It.IsAny<Expression<Func<HomeUser, bool>>>()), Times.Once);
+    }
+}
