95d170d7a587af10546c4ae2d1643a13d462e944
juanpeyrot <106559717+juanpeyrot@users.noreply.github.com>
2024-10-03 11:39:44 -0300

    green getByValue permission service

---
+++ Homify.BusinessLogic/Homes/HomePermissionService.cs
@@ -0,0 +1,19 @@
+﻿using Homify.BusinessLogic.Homes.Entities;
+using Homify.DataAccess.Repositories;
+
+namespace Homify.BusinessLogic.Homes;
+
+public class HomePermissionService : IHomePermissionService
+{
+    private readonly IRepository<HomePermission> _repository;
+
+    public HomePermissionService(IRepository<HomePermission> repository)
+    {
+        _repository = repository;
+    }
+
+    public HomePermission? GetByValue(string value)
+    {
+        return _repository.Get(x => x.Value == value);
+    }
+}
---
+++ Homify.BusinessLogic/Homes/IHomePermissionService.cs
@@ -0,0 +1,8 @@
+﻿using Homify.BusinessLogic.Homes.Entities;
+
+namespace Homify.BusinessLogic.Homes;
+
+public interface IHomePermissionService
+{
+    HomePermission? GetByValue(string value);
+}
