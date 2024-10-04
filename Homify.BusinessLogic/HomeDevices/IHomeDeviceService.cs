24a83361facf182d5aec89ec966768b41ad11d91
juanpeyrot <106559717+juanpeyrot@users.noreply.github.com>
2024-10-04 11:27:19 -0300

    pre migration

--- Homify.BusinessLogic/HomeDevices/IHomeDeviceService.cs
+++ Homify.BusinessLogic/HomeDevices/IHomeDeviceService.cs
@@ -2,7 +2,6 @@ using Homify.BusinessLogic.Devices;
 using Homify.BusinessLogic.Homes.Entities;
 
 namespace Homify.BusinessLogic.HomeDevices;
-
 public interface IHomeDeviceService
 {
     HomeDevice AddHomeDevice(Home home, Device device);
