2f5f5a8ed49b8cb2ddedba3a9988c2ccd3858115
juanpeyrot <106559717+juanpeyrot@users.noreply.github.com>
2024-10-23 00:30:39 -0300

    angular setup

---
+++ Homify.Angular/src/app/pages/admin-page/admin-routing.module.ts
@@ -0,0 +1,27 @@
+// import { NgModule } from '@angular/core';
+// import { RouterModule, Routes } from '@angular/router';
+
+// import { AdministratorPageComponent } from './administrator-page/administrator-page.component';
+
+// import { administratorGuard } from '../guards/administrator.guard';
+
+
+// const routes: Routes = [
+//   {
+//     path: '',
+//     canActivate: [administratorGuard],
+//     component: AdministratorPageComponent,
+//     children: [
+//       {
+//         path: 'create',
+//         component: CreateAdministratorPageComponent,
+//       },
+//     ]
+//   }
+// ];
+
+// @NgModule({
+//   imports: [RouterModule.forChild(routes)],
+//   exports: [RouterModule]
+// })
+// export class AdministratorRoutingModule { }
