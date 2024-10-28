902820d8c296e4c51379b668901864d0902a116e
Guillermo Dotti <diottiguillermo@gmail.com>
2024-10-28 16:56:45 -0300

    refactor UpdateHomeDevice_WhenRequestIsNull_ThrowsException

--- Homify.Tests/ControllerTests/HomeDeviceControllerTest.cs
+++ Homify.Tests/ControllerTests/HomeDeviceControllerTest.cs
@@ -6,6 +6,13 @@ namespace Homify.Tests.ControllerTests;
 [TestClass]
 public class HomeDeviceControllerTest
 {
+    private readonly HomeDeviceController _controller;
+
+    public HomeDeviceControllerTest()
+    {
+        _controller = new HomeDeviceController();
+    }
+
     [TestMethod]
     [ExpectedException(typeof(NullRequestException))]
     public void UpdateHomeDevice_WhenRequestIsNull_ThrowsException()
