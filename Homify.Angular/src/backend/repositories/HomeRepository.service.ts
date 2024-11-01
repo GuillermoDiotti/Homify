f7173f31824f7e9ac11f71305573b9d40d9b64ef
Nicolas Toscano <toscanico3@gmail.com>
2024-11-01 19:54:20 -0300

    create home

---
+++ Homify.Angular/src/backend/repositories/HomeRepository.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable, throwError } from "rxjs";
+import { catchError } from "rxjs/operators";
+import { environment } from "../../environment";
+import ApiRepository from "./api-repository";
+import { CreateHomeRequest } from "../services/homes/models/CreateHomeRequest";
+import { CreateHomeResponse } from "../services/homes/models/CreateHomeResponse";
+
+
+@Injectable({
+    providedIn: "root",
+  })
+  export class HomeApiRepositoryService extends ApiRepository{    
+    constructor(http: HttpClient) {
+        super(environment.homifyApi, 'homes', http);
+      }
+    
+      public create(request: CreateHomeRequest): Observable<CreateHomeResponse> {
+        return this.post<CreateHomeResponse>(request).pipe(catchError(this.handleError));
+      }
+    
+    }
