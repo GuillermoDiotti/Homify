87b78500eb2db5924aac4619b577e6c1c7af7814
Nicolas Toscano <toscanico3@gmail.com>
2024-11-01 19:57:39 -0300

    add home servce

---
+++ Homify.Angular/src/backend/services/homes/home.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HomeApiRepositoryService } from '../../repositories/HomeRepository.service';
+import { CreateHomeRequest } from './models/CreateHomeRequest';
+import { CreateHomeResponse } from './models/CreateHomeResponse';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+
+  constructor(private readonly _repository: HomeApiRepositoryService) { }
+
+  public create(request: CreateHomeRequest): Observable<CreateHomeResponse> {
+    return this._repository.create(request);
+  }
+}
