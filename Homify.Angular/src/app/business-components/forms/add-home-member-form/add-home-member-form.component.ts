b8525fbbb9c85eb9efb47da8a2c6c2ebfa0d6817
Nicolas Toscano <toscanico3@gmail.com>
2024-11-02 12:34:41 -0300

    add home member form

---
+++ Homify.Angular/src/app/business-components/forms/add-home-member-form/add-home-member-form.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HomeService } from '../../../../backend/services/homes/home.service';
+import { UpdateMemberListRequest } from '../../../../backend/services/homes/models/UpdateMemberListRequest';
+import { APIError } from '../../../../interfaces/interfaces';
+
+@Component({
+  selector: 'app-add-home-member-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-home-member-form.component.html',
+  styleUrl: './add-home-member-form.component.css'
+})
+export class AddHomeMemberFormComponent {
+  form: FormGroup;
+	successMessage = '';
+	errorMessage = '';
+
+  constructor(private fb: FormBuilder, private HomeService: HomeService) {
+    this.form = this.fb.group({
+      email: ["", [Validators.required]],
+    });
+  }
+
+  handleSubmit() {
+		this.successMessage = '';
+		this.errorMessage = '';
+    if (this.form.valid) {
+      const { email } = this.form.value;
+      const req: UpdateMemberListRequest = {
+        email
+      };
+
+      this.HomeService.updateMembers(req).subscribe(
+        response => {
+         	this.successMessage = "Member added successfully";
+        },
+        (error: APIError) => {
+          this.errorMessage = error.error.message;
+        }
+      );
+    } else {
+      this.errorMessage = "Form is invalid";
+    }
+  }
+}
--- Homify.Angular/src/app/business-components/lists/owner-home-list/owner-home-list.component.ts
+++ Homify.Angular/src/app/business-components/lists/owner-home-list/owner-home-list.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import { GetAllHomesResponse } from '../../../../backend/services/homes/models/GetAllHomesResponse';
 import { HomeService } from '../../../../backend/services/homes/home.service';
+import { ButtonComponent } from '../../../components/button/button.component';
 
 @Component({
   selector: 'app-owner-home-list',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './owner-home-list.component.html',
   styleUrl: './owner-home-list.component.css'
 })
 export class OwnerHomeListComponent {
   homes: GetAllHomesResponse[] = [];
+  selectedHomeId = "";
 	constructor(private HomeService: HomeService){}
 
 	ngOnInit() {
@@ -33,5 +35,10 @@ export class OwnerHomeListComponent {
 				console.error("Error listing homes", error);
 			}
 		)
+	}
+
+	onClick(id:string){
+		this.selectedHomeId = id;
+		console.log(id);
 	}
 }
