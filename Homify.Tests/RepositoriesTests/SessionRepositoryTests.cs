99df5462e67e3c513b102b9a5cdbc59cedd25151
Guillermo Dotti <diottiguillermo@gmail.com>
2024-11-03 12:04:12 -0300

    refactor green test Get_UserExists_ReturnsSession

--- Homify.Tests/RepositoriesTests/SessionRepositoryTests.cs
+++ Homify.Tests/RepositoriesTests/SessionRepositoryTests.cs
@@ -1,6 +1,8 @@
 using System.Linq.Expressions;
 using Homify.BusinessLogic.Roles;
+using Homify.BusinessLogic.Roles.Entities;
 using Homify.BusinessLogic.Sessions.Entities;
+using Homify.BusinessLogic.UserRoles.Entities;
 using Homify.BusinessLogic.Users.Entities;
 using Homify.DataAccess.Repositories;
 using Homify.Exceptions;
@@ -9,7 +11,7 @@ using Moq;
 
 namespace Homify.Tests.RepositoriesTests;
 
-/*[TestClass]
+[TestClass]
 public class SessionRepositoryTests
 {
     [TestMethod]
@@ -27,7 +29,10 @@ public class SessionRepositoryTests
                     Name = "User1",
                     LastName = "LastName1",
                     Password = "Password1",
-                    Roles = RolesGenerator.Admin()
+                    Roles =
+                    [
+                        new UserRole { Role = new Role { Name = "ADMINISTRATOR" } }
+                    ]
                 }
             }
         }.AsQueryable();
@@ -50,7 +55,7 @@ public class SessionRepositoryTests
         Assert.AreEqual("User1", result.User.Name);
         Assert.AreEqual("LastName1", result.User.LastName);
         Assert.AreEqual("user1@example.com", result.User.Email);
-        Assert.AreEqual("ADMINISTRATOR", result.User.Roles.Name);
+        Assert.AreEqual("ADMINISTRATOR", result.User.Roles.First().Role.Name);
     }
 
     [TestMethod]
@@ -70,4 +75,4 @@ public class SessionRepositoryTests
 
         Assert.ThrowsException<NotFoundException>(() => sessionRepository.Get(predicate));
     }
-}*/
+}
