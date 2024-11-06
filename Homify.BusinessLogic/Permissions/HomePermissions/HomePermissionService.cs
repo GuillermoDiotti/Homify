ed2fc029c6970d67e5a724e47b44ae8115f5f486
Guillermo Dotti <diottiguillermo@gmail.com>
2024-11-06 18:23:18 -0300

    green ChangeHomeMemberPermissions_WhenUserIsNotOwner_ShouldThrowInvalidOperationException

--- Homify.BusinessLogic/Permissions/HomePermissions/HomePermissionService.cs
+++ Homify.BusinessLogic/Permissions/HomePermissions/HomePermissionService.cs
@@ -1,4 +1,6 @@
-﻿using Homify.BusinessLogic.Permissions.HomePermissions.Entities;
+﻿using Homify.BusinessLogic.HomeUsers;
+using Homify.BusinessLogic.Permissions.HomePermissions.Entities;
+using Homify.BusinessLogic.Users.Entities;
 using Homify.DataAccess.Repositories;
 
 namespace Homify.BusinessLogic.Permissions.HomePermissions;
@@ -15,5 +17,34 @@ public class HomePermissionService : IHomePermissionService
     public HomePermission? GetByValue(string value)
     {
         return _repository.Get(x => x.Value == value);
+    }
+
+    public List<HomePermission> ChangeHomeMemberPermissions(bool addDevice, bool listDevice, User user, HomeUser? found)
+    {
+        if (user.Id != found.Home.OwnerId)
+        {
+            throw new InvalidOperationException("You must be the owner of this home");
+        }
+
+        var list = new List<HomePermission>();
+        if (addDevice)
+        {
+            var permission = GetByValue(PermissionsGenerator.MemberCanAddDevice);
+            if (permission != null)
+            {
+                list.Add(permission);
+            }
+        }
+
+        if (listDevice)
+        {
+            var permission = GetByValue(PermissionsGenerator.MemberCanListDevices);
+            if (permission != null)
+            {
+                list.Add(permission);
+            }
+        }
+
+        return list;
     }
 }
--- Homify.Tests/ServiceTests/HomePermissionTest.cs
+++ Homify.Tests/ServiceTests/HomePermissionTest.cs
@@ -1,6 +1,9 @@
 ﻿using System.Linq.Expressions;
+using Homify.BusinessLogic.Homes.Entities;
+using Homify.BusinessLogic.HomeUsers;
 using Homify.BusinessLogic.Permissions.HomePermissions;
 using Homify.BusinessLogic.Permissions.HomePermissions.Entities;
+using Homify.BusinessLogic.Users.Entities;
 using Homify.DataAccess.Repositories;
 using Moq;
 
@@ -42,5 +45,24 @@ public class HomePermissionTest
         var result = _service.GetByValue("nonExistingValue");
 
         Assert.IsNull(result);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void ChangeHomeMemberPermissions_WhenUserIsNotOwner_ShouldThrowInvalidOperationException()
+    {
+        var homeId = "home123";
+        var memberId = "member123";
+        var user = new User { Id = "notOwner123" };
+        var home = new Home { Id = homeId, OwnerId = "owner123" };
+        var found = new HomeUser { Home = home, UserId = memberId, Permissions = new List<HomePermission>() };
+
+        var _homeUserServiceMock = new Mock<IHomeUserService>();
+        var _repositoryMock = new Mock<IRepository<HomePermission>>();
+        var _service = new HomePermissionService(_repositoryMock.Object);
+
+        _homeUserServiceMock.Setup(service => service.GetByIds(homeId, memberId)).Returns(found);
+
+        _service.ChangeHomeMemberPermissions(true, true, user, found);
     }
 }
