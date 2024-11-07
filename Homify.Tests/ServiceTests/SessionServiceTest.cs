317937937fd126aa1a1535e5facb0ac5ebcdbb1a
Guillermo Dotti <diottiguillermo@gmail.com>
2024-11-07 10:14:30 -0300

    green CheckSessionConstraints_WhenValid_ShouldReturnUser

--- Homify.Tests/ServiceTests/SessionServiceTest.cs
+++ Homify.Tests/ServiceTests/SessionServiceTest.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq.Expressions;
 using Homify.BusinessLogic.Sessions;
 using Homify.BusinessLogic.Sessions.Entities;
+using Homify.BusinessLogic.Users;
 using Homify.BusinessLogic.Users.Entities;
 using Homify.DataAccess.Repositories;
 using Homify.Exceptions;
@@ -12,14 +13,15 @@ namespace Homify.Tests.ServiceTests;
 public class SessionServiceTest
 {
     private Mock<IRepository<Session>>? _sessionRepositoryMock;
-
+    private Mock<IUserService>? _userServiceMock;
     private SessionService? _service;
 
     [TestInitialize]
     public void Setup()
     {
+        _userServiceMock = new Mock<IUserService>();
         _sessionRepositoryMock = new Mock<IRepository<Session>>();
-        _service = new SessionService(_sessionRepositoryMock.Object);
+        _service = new SessionService(_sessionRepositoryMock.Object, _userServiceMock.Object);
     }
 
     [TestMethod]
@@ -92,5 +94,89 @@ public class SessionServiceTest
         Assert.IsNotNull(result.AuthToken);
         _sessionRepositoryMock.Verify(repo => repo.Get(It.IsAny<Expression<Func<Session, bool>>>()), Times.Once);
         _sessionRepositoryMock.Verify(repo => repo.Add(It.IsAny<Session>()), Times.Once);
+    }
+
+    /*[TestMethod]
+    [ExpectedException(typeof(InvalidFormatException))]
+    public void Create_WhenEmailFormatIsInvalid_ShouldThrowInvalidFormatException()
+    {
+        var request = new CreateSessionRequest
+        {
+            Email = "invalid-email",
+            Password = "testPassword"
+        };
+
+        _service.CheckSessionConstraints(request.Email, request.Password);
+    }*/
+
+    /*[TestMethod]
+    [ExpectedException(typeof(ArgsNullException))]
+    public void Create_WhenEmailIsNull_ShouldThrowArgsNullException()
+    {
+        var request = new CreateSessionRequest
+        {
+            Email = null,
+            Password = "testPassword"
+        };
+
+        _service.CheckSessionConstraints(request.Email, request.Password);
+    }*/
+
+    /*[TestMethod]
+    [ExpectedException(typeof(InvalidFormatException))]
+    public void Create_WhenPasswordIsIncorrect_ShouldThrowArgumentException()
+    {
+        var request = new CreateSessionRequest
+        {
+            Email = "user@example.com",
+            Password = "wrongPassword"
+        };
+
+        var user = new User
+        {
+            Email = "user@example.com",
+            Password = ".Coo120dshjha"
+        };
+
+        _userServiceMock
+            .Setup(service => service.GetAll(It.IsAny<string?>(), It.IsAny<string?>()))
+            .Returns([user]);
+
+        _service.CheckSessionConstraints(request.Email, request.Password);
+    }*/
+
+    /*[TestMethod]
+    [ExpectedException(typeof(NotFoundException))]
+    public void Create_WhenUserNotFound_ShouldThrowNotFoundException()
+    {
+        var request = new CreateSessionRequest
+        {
+            Email = "nonexistent@example.com",
+            Password = "testPassword"
+        };
+
+        _userServiceMock
+            .Setup(service => service.GetAll(It.IsAny<string?>(), It.IsAny<string?>()))
+            .Returns([]);
+
+        _service.CheckSessionConstraints(request.Email, request.Password);
+    }*/
+
+    [TestMethod]
+    public void CheckSessionConstraints_WhenValid_ShouldReturnUser()
+    {
+        var email = "example@mail.com";
+        var password = "password123.";
+
+        var user = new User()
+        {
+            Email = email, Password = password
+        };
+        _userServiceMock.Setup(u => u.GetAll(It.IsAny<string?>(), It.IsAny<string?>())).Returns([user]);
+
+        var result = _service.CheckSessionConstraints(email, password);
+
+        Assert.IsNotNull(result);
+        Assert.AreEqual(user, result);
     }
 }
