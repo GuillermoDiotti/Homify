c7ecf182be80d1d161e44155df40d63407b73b8f
juanpeyrot <106559717+juanpeyrot@users.noreply.github.com>
2024-11-08 21:25:19 -0300

    comp owner guard

--- Homify.Angular/src/app/pages/company-page/company.module.ts
+++ Homify.Angular/src/app/pages/company-page/company.module.ts
@@ -6,10 +6,10 @@ import { CreateCompanyPageComponent } from './create/companyList-page.component'
 import { CreateCompanyFormComponent } from '../../business-components/forms/create-companies-form/create-company-form.component';
 import { CompaniesListComponent } from '../../business-components/lists/companies-list/companies-list.component';
 import { AdminGuard } from '../../guards/admin.guard';
+import { CompanyOwnerGuard } from '../../guards/company-owner.guard';
 
 const routes: Routes = [
-  { path: '', component: CompanyListPageComponent },
-  { path: 'create', component: CreateCompanyPageComponent },
+  { path: 'create', component: CreateCompanyPageComponent, canActivate: [CompanyOwnerGuard] },
   { path: 'list', component: CompanyListPageComponent, canActivate: [AdminGuard] }
 ];
 
