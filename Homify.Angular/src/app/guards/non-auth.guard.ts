b44970089d8e0fa3fdf4c490c348229b0baa18d7
juanpeyrot <106559717+juanpeyrot@users.noreply.github.com>
2024-11-08 21:48:46 -0300

    non auth guard

---
+++ Homify.Angular/src/app/guards/non-auth.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { HomifyConstants } from '../../utility/HomifyConstants';
+import { SessionService } from '../../backend/services/session/Session.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NonAuthGuard implements CanActivate {
+
+  constructor(private router: Router, private readonly SessionService: SessionService) {}
+
+  canActivate(): boolean {
+		const { token, roles } = this.SessionService.getCurrentUser(); 
+ 
+    if (!token) {
+      return true;
+    } else {
+      this.router.navigate(['/']);
+      return false;
+    }
+  }
+}
--- Homify.Angular/src/app/pages/device-page/device.module.ts
+++ Homify.Angular/src/app/pages/device-page/device.module.ts
@@ -8,9 +8,10 @@ import { AllDevicesComponent } from './all-devices/all-devices.component';
 import { SupportedDevicesListComponent } from '../../business-components/lists/supported-devices-list/supported-devices-list.component';
 import { RegisteredDevicesListComponent } from '../../business-components/lists/registered-devices-list/registered-devices-list.component';
 import { AuthGuard } from '../../guards/auth.guard';
+import { CompanyOwnerGuard } from '../../guards/company-owner.guard';
 
 const routes: Routes = [
-  { path: '', component: DevicePageComponent },
+  { path: '', component: DevicePageComponent, canActivate: [CompanyOwnerGuard] },
 	{ path: 'supported', component: SupportedDevicesComponent, canActivate: [AuthGuard] },
 	{ path: 'all', component: AllDevicesComponent, canActivate: [AuthGuard] },
 ];
