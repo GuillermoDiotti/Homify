2ff6c4cec221daaa6d5e61d228cc108a48386fda
Nicolas Toscano <toscanico3@gmail.com>
2024-11-19 12:46:01 -0300

    green lamp off null

--- Homify.Tests/ServiceTests/HomeDeviceServiceTest.cs
+++ Homify.Tests/ServiceTests/HomeDeviceServiceTest.cs
@@ -300,6 +300,18 @@ public class HomeDeviceServiceTest
     }
 
     [TestMethod]
+    [ExpectedException(typeof(NotFoundException))]
+    public void LampOff_WhenDeviceIsNull_ShouldThrowException()
+    {
+        var hardwareId = "test-hardware-id";
+
+        _homeDeviceRepositoryMock.Setup(repo => repo.Get(It.IsAny<Expression<Func<HomeDevice, bool>>>()))
+            .Returns((HomeDevice)null);
+
+        var result = _homeDeviceService.LampOff(hardwareId);
+    }
+
+    [TestMethod]
     public void LampOff_WhenIsOk_ShouldTurnOffLampAndReturnHomeDevice()
     {
         var hardwareId = "test-hardware-id";
